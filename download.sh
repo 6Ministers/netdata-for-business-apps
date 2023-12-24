6a0b145fcace616917622393b7f0da23e5af0dac
Eduard <markerbiznes@gmail.com>
2023-12-24 19:08:48 +0300

    Update

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 DESTINATION=$1
 
 # clone Flectra directory
-git clone --depth=1 https://github.com/6Ministers/onlyoffice-for-nextcloude $DESTINATION
+git clone --depth=1 https://github.com/6Ministers/netdata-for-business-apps $DESTINATION
 rm -rf $DESTINATION/.git
 
 
